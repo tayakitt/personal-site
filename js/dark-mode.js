97fa17c6e018dfaa42e5a354d131be292ce6b130
Taya Kitt <taya.kitt@gmail.com>
2019-08-06 00:27:17 -0400

    add dark grey colour

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
-  console.log("sceipagf");
+})
+
+document.getElementById("moon").addEventListener("click", function(){
+  localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
+  
+  if (localStorage.getItem('mode') === 'dark') {
+    document.querySelector('body').classList.add('dark');
+  } else {
+    document.querySelector('body').classList.remove('dark')
+  }
 })
